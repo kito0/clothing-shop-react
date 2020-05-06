d5cdf6d799160e7809b67c4c434f4bf8aba1703e
Andrew X. Shah <drewshah0@gmail.com>
2020-05-06 02:55:26 -0600

    initialized pages

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,9 @@
 import React from 'react';
-import './App.css';
+import './styles/App.css';
 
 function App() {
   return (
     <div className="App">
-      <HomePage/>
     </div>
   );
 }
--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,1 +1,8 @@
-ÿþ
+import React from 'react';
+import './styles/HomePage.css';
+
+const HomePage = () => {
+    
+}
+
+export default HomePage;
