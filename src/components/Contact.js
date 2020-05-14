8a47559e5d58697d9026dc1213fe7ef58c032266
The-Real-Guy <aasdimaranan@gmail.com>
2020-05-13 19:23:38 -0600

    Started contact page. FAQ unfinished

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -3,8 +3,14 @@ import './styles/Contact.css';
 
 const Contact = () => (
     <div className="contact">
-        <h1>Contact</h1>
-        <h2>Sizing, stock, and refunds: </h2>
+        <img className="contact-img" src={require("../assets/contactBG.JPG")} alt="contact"/>
+        <div className="contact-block">
+            <h1 className="contact-txt">Contact</h1>
+            <h2 className="contact-item">For sizing, stock inquiries, or refunds: info@wearsoup.com</h2>
+            <h2 className="contact-item">For business inquiries: business@wearsoup.com</h2>
+            <h2 className="contact-item">For wholesale inquiries or opportunities: wholesale@wearsoup.com</h2>
+            <h1 className="contact-txt">FAQs</h1>
+        </div>
     </div>
 );
 
